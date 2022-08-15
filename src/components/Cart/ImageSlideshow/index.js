e40175fa515a34ef42ac125650e6721128d385ea
OsykaOleksandr <osykaaleksandr8@gmail.com>
2022-08-15 12:20:51 +0300

    Remove arrows if there is only one image

--- src/components/Cart/ImageSlideshow/index.js
+++ src/components/Cart/ImageSlideshow/index.js
@@ -2,51 +2,63 @@ import React, { Component } from "react";
 
 import { ReactComponent as ArrowLeft } from "../../../assets/arrowLeft.svg";
 import { ReactComponent as ArrowRight } from "../../../assets/arrowRight.svg";
-import './style.css';
+import "./style.css";
 
 class ImageSlideshow extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {currentSlide: 0}
+  constructor(props) {
+    super(props);
+    this.state = { currentSlide: 0 };
+  }
+
+  increment = () => {
+    const { currentSlide } = this.state;
+    const { images } = this.props;
+
+    if (currentSlide + 1 > images.length - 1) {
+      this.setState({ currentSlide: 0 });
+    } else {
+      this.setState({ currentSlide: currentSlide + 1 });
     }
+  };
 
-    increment = () => {
-        const { currentSlide } = this.state;
-        const { images } = this.props;
+  decrement = () => {
+    const { currentSlide } = this.state;
+    const { images } = this.props;
 
-        if(currentSlide + 1 > images.length - 1) {
-            this.setState({currentSlide: 0});
-        } else {
-            this.setState({currentSlide: currentSlide + 1});
-        }
+    if (currentSlide - 1 < 0) {
+      this.setState({ currentSlide: images.length - 1 });
+    } else {
+      this.setState({ currentSlide: currentSlide - 1 });
     }
+  };
 
-    decrement = () => {
-        const { currentSlide } = this.state;
-        const { images } = this.props;
+  render() {
+    const { images } = this.props;
 
-        if(currentSlide - 1 < 0) {
-            this.setState({currentSlide: images.length - 1});
-        } else {
-            this.setState({currentSlide: currentSlide - 1});
-        }
-    }
+    const { currentSlide } = this.state;
 
-    render() {
-        const { images } = this.props;
-
-        const { currentSlide } = this.state;
-
-        return(
-            <div className="ImageSlideshow_container">
-                <img src={images[currentSlide]}  alt="" />
-                <button className="ImageSlideshow_button ImageSlideshow_buttonPrevious" onClick={this.decrement}><ArrowLeft /></button>
-                <button className="ImageSlideshow_button ImageSlideshow_buttonNext" onClick={this.increment}><ArrowRight /></button>
-            </div>
-        );
-    }
+    return (
+      <div className="ImageSlideshow_container">
+        <img src={images[currentSlide]} alt="" />
+        {images.length > 1 && (
+          <>
+            <button
+              className="ImageSlideshow_button ImageSlideshow_buttonPrevious"
+              onClick={this.decrement}
+            >
+              <ArrowLeft />
+            </button>
+            <button
+              className="ImageSlideshow_button ImageSlideshow_buttonNext"
+              onClick={this.increment}
+            >
+              <ArrowRight />
+            </button>
+          </>
+        )}
+      </div>
+    );
+  }
 }
-
-
 
 export default ImageSlideshow;
